4638ee8b79f673cf0b2d1dc7af1a763831c52f4d
bigjohn98pl-PC <56130048+bigjohn98pl@users.noreply.github.com>
2022-04-27 12:19:21 +0200

    fix, second problem dont show graph

--- main.cpp
+++ main.cpp
@@ -7,24 +7,22 @@ int main()
     static vector<vector<double>> Tab1 = {{999,1,1,2,3},{3,999,2,5,6},{5,4,999,3,7},{8,4,3,999,2},{7,7,5,6,999}};
     static vector<vector<double>> Tab2 = {{999,8,7,5},{2,999,6,4},{3,10,999,4},{7,5,4,999}};
 
-    little problem1(Tab1);
-    little problem2(Tab2);
+    little problem1(Tab2);
+    little problem2(Tab1);
 
     problem1.showArray();
     problem1.stepOne(true);
     problem1.showGraph();
 
-    problem1.~little();
-
     problem2.showArray();
     problem2.stepOne(true);
     problem2.showGraph();
 
-    {
-    little problem3(Tab1);
-    problem3.showArray();
-    problem3.stepOne(true);
-    problem3.showGraph();
-    }
+//    {
+//    little problem3(Tab1);
+//    problem3.showArray();
+//    problem3.stepOne(true);
+//    problem3.showGraph();
+//    }
     return 0;
 }
